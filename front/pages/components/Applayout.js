a64a0ced03f5afb8d70c357a1ea67715f55065e8
원동휘 <donghwi1289@gmail.com>
2021-07-03 17:53:34 +0900

    ADD : 프로젝트 세팅 + 라이브러리 추가

---
+++ front/pages/components/Applayout.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+
+const AppLayout = ({ children }) => {
+  return (
+    <div>
+      <div>
+        <Link href="/">
+          <a>홈</a>
+        </Link>
+        <Link href="/profile">
+          <a>프로필</a>
+        </Link>
+        <Link href="/signup">
+          <a>회원가입</a>
+        </Link>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default AppLayout;
