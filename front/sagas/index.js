540acc91ff134978c3350527662dcc9a461b5018
원동휘 <donghwi1289@gmail.com>
2021-07-08 02:24:28 +0900

    ADD : Redux-Saga 기본세팅

---
+++ front/sagas/index.js
@@ -0,0 +1,83 @@
+import { all, fork, put, call, take } from "redux-saga/effects";
+import axios from "axios";
+
+//all = 배열에 있는부분을 동시에 실행
+//fork = 함수를 실행
+//take = () 액션이 실행될때 까지 기다리겠다
+//call = call해서 ()의 리턴값을 result변수에 받기
+
+//fork - call 차이
+// -> fork : 비동기 함수 호출 (결과값 안기다림) , call : 동기 함수 호출 (결과값을 기다림)
+
+function logInAPI(data) {
+  return axios.post("/api/login", data);
+}
+
+function* logIn(action) {
+  try {
+    const result = yield call(logInAPI, action.data);
+    yield put({
+      type: "LOG_IN_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "LOG_IN_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function logOutAPI() {
+  return axios.post("/api/out");
+}
+
+function* logOut() {
+  try {
+    const result = yield call(logOutAPI);
+    yield put({
+      type: "LOG_OUT_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "LOG_OUT_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function addPostAPI(data) {
+  return axios.post("/api/post", data);
+}
+
+function* addPost(action) {
+  try {
+    const result = yield call(addPostAPI, action.data);
+    yield put({
+      type: "ADD_POST_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "ADD_POST_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function* watchLogIn() {
+  yield take("LOG_IN_REQUEST", logIn);
+}
+
+function* watchLogOut() {
+  yield take("LOG_OUT_REQUEST", logOut);
+}
+
+function* watchAddPost() {
+  yield take("ADD_POST_REQUEST", addPost);
+}
+
+export default function* rootSaga() {
+  yield all([fork(watchLogIn), fork(watchLogOut), fork(watchAddPost)]);
+}
--- front/store/configStore.js
+++ front/store/configStore.js
@@ -1,16 +1,28 @@
 import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, createStore } from "redux";
-import reducer from "../reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
+import reducer from "../reducers";
+import rootSaga from "../sagas";
+
+const loggerMiddleware =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    console.log(action);
+    return next(action);
+  };
 
 const configStore = () => {
-  const middlewares = [];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware, loggerMiddleware];
   const enhancer =
     process.env.NODE_ENV === "poduction"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares));
 
   const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
