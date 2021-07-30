8f8bce7bd6f85ca94df667630ad28466ba1cee52
wondonghwi <donghwi1289@gmail.com>
2021-07-30 16:34:13 +0900

    delete : 안쓰는 코드 삭제

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -60,13 +60,6 @@ export const ADD_COMMENT_FAILURE = "ADD_COMMENT_FAILURE";
 //동기적
 export const REMOVE_IMAGE = "REMOVE_IMAGE";
 
-export const addPost = (data) => {
-  return {
-    type: ADD_POST_REQUEST,
-    data,
-  };
-};
-
 // 이전 상태를 액션을 통해 다음 상태로 만들어내는 함수(불변성은 지키면서)
 const reducer = (state = initialState, action) =>
   produce(state, (draft) => {
