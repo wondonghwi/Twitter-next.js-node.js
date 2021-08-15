fa6c0ebe4166af2775b21a2944c62c8b081a7004
wondonghwi <donghwi1289@gmail.com>
2021-08-15 11:36:38 +0900

    next.js 빌드시 성능 확인을 위한 analyzer 적용

--- front/next.config.js
+++ front/next.config.js
@@ -1,6 +1,20 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+module.exports = withBundleAnalyzer({
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === "production";
+    const plugins = [...config.plugins];
+    return {
+      ...config,
+      mode: prod ? "production" : "development",
+      devtool: prod ? "hidden-source-map" : "eval",
+      plugins,
+    };
+  },
+  compress: true,
   reactStrictMode: true,
   images: {
     domains: ["localhost", "i.ytimg.com", "placeimg.com"],
   },
-};
+});
--- front/pages/_app.js
+++ front/pages/_app.js
@@ -2,7 +2,7 @@ import "antd/dist/antd.css"; //antdesign을 전역으로 적용
 import Head from "next/head";
 import wrapper from "../store/configStore";
 
-//TODO 빌드시 람다 = SSR된 페이지들, 검은동그라미는 SSG, 흰동그라미는 그냥 정적페이지(Html)
+//TODO 빌드시 람다 = SSR된 페이지들, 검은동그라미는 SSG, 흰동그라미는 그냥 정적페이지(Html) ->  용량은 1Mb만 안넘으면 됨
 
 //TODO _app.js는 모든 pages들의 공통 부분
 //next-redux-wrapper 에서 자동으로 Provider store={store}를 해줘서 적으면 안됨!
