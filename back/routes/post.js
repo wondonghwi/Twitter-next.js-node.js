d72cdd93f2f37fa83ef154f2dfac4cf11a885b33
wondonghwi <donghwi1289@gmail.com>
2021-08-24 09:52:13 +0900

    fix : images연결 오류 해결

--- back/routes/post.js
+++ back/routes/post.js
@@ -99,7 +99,7 @@ router.post("/", isLoggedIn, upload.none(), async (req, res, next) => {
 router.post("/images", isLoggedIn, upload.array("image"), (req, res, next) => {
   // POST /post/images
   console.log(req.files);
-  res.json(req.files.map((v) => v.location.replace(/\/original\//, "/thumb/")));
+  res.json(req.files.map((v) => v.location));
 });
 
 router.get("/:postId", async (req, res, next) => {
